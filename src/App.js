17420a67bc0beafbb353b0b74e85ac946d627a3e
fitszjerald <hesamya.ahrari@gmail.com>
2019-05-02 15:30:40 +0430

    add menu with react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Header from './components/section/Header'
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
 
 class App extends React.Component {
   render(){
     return (
-      <div className="App">
-      
+      <div>
+        <Router>
+          <Header/>
+          <div className='container'>
+            <div style={{paddingTop:60}}>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            </div>
+          </div>
+          
+        </Router>  
       </div>
     );
   }
